deb799c266a1ff2fecda688365b1b0155f5107ea
Erwin RA <artmxarea@gmail.com>
2021-02-13 00:50:39 +0700

    swap ts to js

--- ios/RNBoilerPlate/AppDelegate.m
+++ ios/RNBoilerPlate/AppDelegate.m
@@ -38,7 +38,7 @@ static void InitializeFlipper(UIApplication *application) {
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self
                                             launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
-                                                   moduleName:@"hattadev"
+                                                   moduleName:@"RNBoilerPlate"
                                             initialProperties:nil];
 
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f
--- ios/RNBoilerPlateTests/RNBoilerPlateTests.m
+++ ios/RNBoilerPlateTests/RNBoilerPlateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface hattadevTests : XCTestCase
+@interface RNBoilerPlateTests : XCTestCase
 
 @end
 
-@implementation hattadevTests
+@implementation RNBoilerPlateTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
